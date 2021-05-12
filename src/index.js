13e4a4b6b053146d3f5915440b83b162f3e73b1b
Bhavya Tanwar <bhavyatanwar@Bhavyas-MacBook-Air.local>
2021-05-12 20:01:28 +0530

    creating the store

--- src/index.js
+++ src/index.js
@@ -2,7 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App';
+import movies from './reducers';
+import { createStore } from 'redux';
 
+const store = createStore(movies);
 ReactDOM.render(
   <React.StrictMode>
     <App />
