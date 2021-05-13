3ae83168cf219976019985cd30ff8b560d7d1b6b
Bhavya Tanwar <bhavyatanwar@Bhavyas-MacBook-Air.local>
2021-05-13 11:37:20 +0530

    applying middleware

--- src/index.js
+++ src/index.js
@@ -3,10 +3,19 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App';
 import movies from './reducers';
-import { createStore } from 'redux';
+import { createStore,applyMiddleware } from 'redux';
 import rootReducer from './reducers';
 
-const store = createStore(rootReducer);
+const logger = function ({ dispatch,getState }){
+  return function (next){
+    return function(action){
+      //middleware code
+      //console.log('ACTION_TYPE = ',action.type),
+      next(action);
+    }
+  }
+}
+const store = createStore(rootReducer,applyMiddleware(logger));
 // store.dispatch({
 //   type:'ADD_MOVIES',
 //   movies:[{name:'Superman'}]
