3c343fa28287bb8a8eb6da15b82fe4b891cc9e87
Bhavya Tanwar <bhavyatanwar@Bhavyas-MacBook-Air.local>
2021-05-13 11:38:50 +0530

    modifying middleware

--- src/index.js
+++ src/index.js
@@ -6,15 +6,18 @@ import movies from './reducers';
 import { createStore,applyMiddleware } from 'redux';
 import rootReducer from './reducers';
 
-const logger = function ({ dispatch,getState }){
-  return function (next){
-    return function(action){
-      //middleware code
-      //console.log('ACTION_TYPE = ',action.type),
-      next(action);
-    }
-  }
+const logger = ({ dispatch,getState }) => (next) => (action) => {
+  next(action);
 }
+// const logger = function ({ dispatch,getState }){
+//   return function (next){
+//     return function(action){
+//       //middleware code
+//       //console.log('ACTION_TYPE = ',action.type),
+//       next(action);
+//     }
+//   }
+// }
 const store = createStore(rootReducer,applyMiddleware(logger));
 // store.dispatch({
 //   type:'ADD_MOVIES',
